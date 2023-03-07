f12b2383ac5b9517920d54df52402f30470f11cc
Princess Ventures <princessventures@CessysM1Air.local>
2023-03-07 16:32:25 +0800

    Working file

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import FeedbackForm from './components/FeedbackForm.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
+	import FeedbackList from './components/FeedbackList.svelte'
+
+</script>
+
+<main class = "container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+</main>
